566cb9eba2a7819bd286d8a5bc21bc2b818b0ad9
MercurialKara <videogmer314@yahoo.com>
2025-07-23 12:53:41 -0500

    added stats.py, split off the word count function into it

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return(file_contents)
     
-def word_count(book_text):
-    words = []
-    words = book_text.split()
- #   return len(words)
-    return(f"{len(words)} words found in the document")
-
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(word_count(text))
+    print(get_word_count(text))
 
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_word_count(book_text):
+    words = []
+    words = book_text.split()
+ #   return len(words)
+    return(f"{len(words)} words found in the document")
