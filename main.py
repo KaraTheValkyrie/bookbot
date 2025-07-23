062e49514e0c72efdae6715b4cfe04723e43b6d3
MercurialKara <videogmer314@yahoo.com>
2025-07-23 14:07:23 -0500

    added a function to organize the letter counts, updated the output formatting

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from stats import get_word_count, get_letter_counts
+from stats import get_word_count, get_letter_counts, organize_letter_counts, sort_key
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return(file_contents)
     
-def main():
-    text = get_book_text("books/frankenstein.txt")
+def main(input):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {input}...")
+    print("----------- Word Count ----------")
+    
+    text = get_book_text(input)
     print(get_word_count(text))
+    
+    print("--------- Character Count -------")
+        
     letter_counts = get_letter_counts(text)
-    print(letter_counts)
+    sorted_letters = organize_letter_counts(letter_counts)
+    
+    for item in sorted_letters:
+        
+        print(f"{item["char"]}: {item["num"]}")
     
 
-main()
+main("books/frankenstein.txt")
+
--- stats.py
+++ stats.py
@@ -2,14 +2,31 @@ def get_word_count(book_text):
     words = []
     words = book_text.split()
  #   return len(words)
-    return(f"{len(words)} words found in the document")
+    return(f"Found {len(words)} total words")
 
 def get_letter_counts(text):
     text = text.lower()
     letter_counts = {}
+
     for char in text:
         if char in letter_counts:
             letter_counts[char] += 1
         else:
             letter_counts[char] = 1
+
     return(letter_counts)
+
+def sort_key(dictionary):
+    return dictionary["num"]
+
+def organize_letter_counts(dictionary):
+    letter_list = []
+    
+    for key, item in dictionary.items():
+        if key.isalpha():
+            new_dict = {"char": key, "num": item}
+            letter_list.append(new_dict)
+
+    letter_list.sort(reverse=True, key=sort_key)
+    return(letter_list)
+
